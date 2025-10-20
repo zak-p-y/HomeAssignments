42c7ad707e1d76f099c1491e5d695f3422964fb4
zak-p-y <st142062@student.spbu.ru>
2025-10-20 21:08:32 +0300

    upload

---
+++ Assignment1/hello.cpp
@@ -0,0 +1,22 @@
+/*
+* Pavel Zak
+* st142062@student.spbu.ru
+* Home_assignment number one
+*/
+
+#include "hello.h"
+#include <iostream>
+
+void printHello() {
+
+	std::cout << "Hello, world"<< std::endl;
+	while (true) {
+		std::string a;
+		std::getline(std::cin, a);
+		std::cout <<a<< std::endl;
+
+}
+
+
+}
+
